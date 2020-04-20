19a6dc4abc0f0f75ae88020a1424dfe2fe95ee03
Jake Nguyen <jnguy182@ucsc.edu>
2020-04-19 23:40:10 -0700

    Implement Parallax

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,7 +7,14 @@ class Play extends Phaser.Scene {
         // load images/tile sprites
         this.load.image('rocket', './assets/Player.png');
         this.load.image('spaceship', './assets/sheep.png');
-        this.load.image('starfield', './assets/starfield.png')
+
+        // load parallax images
+        this.load.image('sky', './assets/1.png');
+        this.load.image('canyon', './assets/2.png');
+        this.load.image('desert', './assets/3.png');
+        this.load.image('cactus', './assets/4.png');
+        this.load.image('sand', './assets/5.png');
+
         // load spritesheet
         this.load.spritesheet('explosion', './assets/explosion.png', {
             frameWidth: 64,
@@ -18,25 +25,36 @@ class Play extends Phaser.Scene {
     }
 
     create() {
-        // place tile sprite
-        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
-        // white rectangle borders
+        // place tile sprite for parallax
+        this.sky = this.add.tileSprite(0, 0, 640, 480, 'sky').setOrigin(0, 0);
+        this.canyon = this.add.tileSprite(0, 0, 640, 480, 'canyon').setOrigin(0, 0);
+        this.desert = this.add.tileSprite(0, 0, 640, 480, 'desert').setOrigin(0, 0);
+        this.cactus = this.add.tileSprite(0, 0, 640, 480, 'cactus').setOrigin(0, 0);
+        this.sand = this.add.tileSprite(0, 0, 640, 480, 'sand').setOrigin(0, 0);
+
+        //
+        /* white rectangle borders
         this.add.rectangle(5, 5, 630, 32, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(5, 443, 630, 32, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(5, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(603, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
         // green UI background
         this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0, 0);
+        */
+
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width / 2 - 8, 431, 'rocket').setScale(0.01, 0.01).setOrigin(0, 0);
+
         // add spaceships (x3)
         this.ship01 = new Spaceship(this, game.config.width + 192, 132, 'spaceship', 0, 30).setScale(0.01, 0.01);
         this.ship02 = new Spaceship(this, game.config.width + 96, 196, 'spaceship', 0, 20).setScale(0.01, 0.01).setOrigin(0, 0);
         this.ship03 = new Spaceship(this, game.config.width, 260, 'spaceship', 0, 10).setScale(0.01, 0.01).setOrigin(0, 0);
+
         // define keys
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
         // animation config
         this.anims.create({
             key: 'explode',
@@ -47,8 +65,10 @@ class Play extends Phaser.Scene {
             }),
             frameRate: 30
         });
+
         // score
         this.p1Score = 0;
+
         // score display
         let scoreConfig = {
             fontFamily: 'Courier',
@@ -63,8 +83,10 @@ class Play extends Phaser.Scene {
             fixedWidth: 100
         }
         this.scoreLeft = this.add.text(69, 54, this.p1Score, scoreConfig);
+
         // game over flag
         this.gameOver = false;
+
         // 60-second play clock
         scoreConfig.fixedWidth = 0;
         this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
@@ -82,8 +104,14 @@ class Play extends Phaser.Scene {
         if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)) {
             this.scene.start("menuScene");
         }
+
         // scroll tile sprite
-        this.starfield.tilePositionX -= 4;
+        this.sky.tilePositionX += 0.5;
+        this.canyon.tilePositionX += 1;
+        this.desert.tilePositionX += 1.5;
+        this.cactus.tilePositionX += 1.5;
+        this.sand.tilePositionX += 1.5;
+
         if (!this.gameOver) {
             // update rocket sprite
             this.p1Rocket.update();
@@ -92,6 +120,7 @@ class Play extends Phaser.Scene {
             this.ship02.update();
             this.ship03.update();
         }
+
         // check collisions
         if (this.checkCollision(this.p1Rocket, this.ship03)) {
             console.log('kaboom ship 03');
@@ -113,10 +142,10 @@ class Play extends Phaser.Scene {
 
     checkCollision(rocket, ship) {
         // simple AABB checking
-        if (rocket.x < ship.x + ship.width*0.01 &&
-            rocket.x + rocket.width*0.01 > ship.x &&
-            rocket.y < ship.y + ship.height*0.01 &&
-            rocket.height*0.01 + rocket.y > ship.y) {
+        if (rocket.x < ship.x + ship.width * 0.01 &&
+            rocket.x + rocket.width * 0.01 > ship.x &&
+            rocket.y < ship.y + ship.height * 0.01 &&
+            rocket.height * 0.01 + rocket.y > ship.y) {
             return true;
         } else {
             return false;
@@ -126,10 +155,13 @@ class Play extends Phaser.Scene {
     shipExplode(ship) {
         // temporarily hide ship
         ship.alpha = 0;
+
         // create explosion sprite at ship's position
         let boom = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0, 0);
+
         // play explode animation
         boom.anims.play('explode');
+
         // callback after animation completes
         boom.on('animationcomplete', () => {
             // reset ship position
@@ -139,6 +171,7 @@ class Play extends Phaser.Scene {
             // remove explosion sprite
             boom.destroy();
         });
+
         // score increment and repaint
         this.p1Score += ship.points;
         this.scoreLeft.text = this.p1Score;
